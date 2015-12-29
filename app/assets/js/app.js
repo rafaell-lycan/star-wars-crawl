b9590c66be6dc823d52d3d25890c07e61bcebc9c
Rafaell Lycan <sonny.webdsg@gmail.com>
2015-12-29 10:17:04 -0200

    Done sound and scene sync

---
+++ app/assets/js/app.js
@@ -0,0 +1,15 @@
+(function  () {
+  var animation = document.querySelector('.animation');
+  var movies = document.querySelector('.movies');
+
+  movies.classList.add('hidden');
+  animation.classList.remove('hidden');
+
+  // Covers
+
+  // Select History
+
+  // Play Music
+  var audio = document.querySelector('audio');
+  audio.play();
+})();
